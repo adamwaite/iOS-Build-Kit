5136b80e45cd10e8ebeb1ff8835eefbc68c3dc60
Denis Lebedev <d2.lebedev@gmail.com>
2014-02-18 12:15:18 +0300

    Remove unnecessary trailing slash from config

--- lib/ios_build_kit/utils/report_creator.rb
+++ lib/ios_build_kit/utils/report_creator.rb
@@ -9,8 +9,11 @@ module BuildKit
 		  def self.create_report runner
 		    to_write = { time: Time.now.to_s, config: runner.config.to_h, preferences: runner.preferences.to_h, runtime_store: runner.store, outputs: {} }
 		    runner.tasks[:run].each { |task| to_write[:outputs][task.keys.first] = runner.outputs[task.keys.first] }
-		    file_name = "#{runner.preferences.reports}report-#{Time.now.to_i}.json"
+		    file_name = File.join(runner.preferences.reports, "report-#{Time.now.to_i}.json")
 		    File.open(file_name, 'w') { |f| f.write to_write.to_json }
+		    puts "DEBUG"
+		    puts runner.preferences.reports
+		    puts file_name
 		  	file_name
 		  end
 
