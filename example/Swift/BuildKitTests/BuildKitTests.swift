7ce8f10564d9980b63ef169a21e8d2b270e08d94
Adam Waite <adam@adamjwaite.co.uk>
2014-09-30 17:56:26 +0100

    Swift project and buildit working in harmony

---
+++ example/Swift/BuildKitTests/BuildKitTests.swift
@@ -0,0 +1,29 @@
+//
+//  BuildKitTests.swift
+//  BuildKitTests
+//
+//  Created by Adam Waite on 30/09/2014.
+//  Copyright (c) 2014 BuildKit. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BuildKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+}
